cb65fb65d8b281f1bd623a995bbf13aa27396c96
ummehani <ummehani@nurturelabs.co>
2022-11-27 15:58:47 +0530

    added more flows

--- abb-contracts/tests/test_abb.py
+++ abb-contracts/tests/test_abb.py
@@ -9,9 +9,10 @@ def test_abb_staking(contracts, accounts, chain):
     bfr_deployer = accounts[1]
     staking_contract = contracts["staking_contract"]
     user_1 = accounts[2]
+    user_2 = accounts[3]
     ONE_DAY = 86400
-    transfer_amount = 1000 * int(1e18)
-    amount = 100 * int(1e18)
+    transfer_amount = 100000 * int(1e18)
+    amount_1 = 100 * int(1e18)
     solana_address = "string"
     apy = {
         "30": 0.05,
@@ -54,19 +55,19 @@ def test_abb_staking(contracts, accounts, chain):
         ) == 0
         assert tokenX.balanceOf(user_1) == 0
 
-    def test_initital_set_up(lock_up_period, amount_to_stake):
-        assert tokenX.balanceOf(user_1) == 0
+    def test_initital_set_up(amount_to_stake, user):
+        lock_up_period = 30
+        assert tokenX.balanceOf(user) == 0
         initial_deployer_balance = tokenX.balanceOf(bfr_deployer)
 
-        print(tokenX.balanceOf(user_1), amount_to_stake)
         with brownie.reverts(""):  # ERC20: transfer amount exceeds balance
             staking_contract.stake(
                 amount_to_stake, lock_up_period, solana_address, {
-                    "from": user_1}
+                    "from": user}
             )
-        tokenX.transfer(user_1, amount_to_stake, {"from": bfr_deployer})
-        initial_tokenX_balance_user_1 = tokenX.balanceOf(user_1)
-        assert tokenX.balanceOf(user_1) == amount_to_stake
+        tokenX.transfer(user, amount_to_stake, {"from": bfr_deployer})
+        initial_tokenX_balance_user_1 = tokenX.balanceOf(user)
+        assert tokenX.balanceOf(user) == amount_to_stake
         assert tokenX.balanceOf(
             bfr_deployer) == initial_deployer_balance - amount_to_stake
 
@@ -74,18 +75,18 @@ def test_abb_staking(contracts, accounts, chain):
         with brownie.reverts(''):  # "ERC20: transfer amount exceeds allowance"
             staking_contract.stake(
                 amount_to_stake, lock_up_period, solana_address, {
-                    "from": user_1}
+                    "from": user}
             )
         tokenX.approve(staking_contract.address,
-                       amount_to_stake, {"from": user_1})
+                       amount_to_stake, {"from": user})
 
-    def test_staking_flow(lock_up_period, amount_to_stake):
+    def test_staking_flow(lock_up_period, amount_to_stake, user):
         initial_tokenX_balance_of_staking_contract = tokenX.balanceOf(
             staking_contract.address
         )
         initial_total_staked_amount = staking_contract.totalStakedAmount()
         stake_transaction = staking_contract.stake(
-            amount_to_stake, lock_up_period, solana_address, {"from": user_1}
+            amount_to_stake, lock_up_period, solana_address, {"from": user}
         )
 
         assert tokenX.balanceOf(
@@ -93,64 +94,62 @@ def test_abb_staking(contracts, accounts, chain):
         assert staking_contract.totalStakedAmount() == initial_total_staked_amount + \
             amount_to_stake
 
-        assert stake_transaction.events["Stake"]["account"] == user_1
+        assert stake_transaction.events["Stake"]["account"] == user
         assert stake_transaction.events["Stake"]["amount"] == amount_to_stake
         assert stake_transaction.events["Stake"]["lockUpPeriod"] == lock_up_period
         assert stake_transaction.events["Stake"]["solanaAddress"] == solana_address
 
-    def test_rewards(lock_up_period, amount_to_stake):
-        # TODO: Removed a view from calculateUserReward function since the chain.sleep does not work on view txns
+    def test_rewards(lock_up_period, amount_to_stake, user):
         chain.snapshot()
         assert (
-            staking_contract.calculateUserReward(user_1) == 0
+            staking_contract.calculateUserReward(user) == 0
         ), "Wrong user reward"
         chain.sleep(int(lock_up_period/2) * ONE_DAY)
         chain.mine(2)
         assert isclose(
-            staking_contract.calculateUserReward(user_1),
+            staking_contract.calculateUserReward(user),
             (apy[str(lock_up_period)] * amount_to_stake * lock_up_period/2) / 365,
             abs_tol=10,
         )
         chain.sleep(lock_up_period * ONE_DAY)
         chain.mine(2)
         assert isclose(
-            staking_contract.calculateUserReward(user_1),
+            staking_contract.calculateUserReward(user),
             (apy[str(lock_up_period)] * amount_to_stake * lock_up_period) / 365,
             abs_tol=10,
         )
         chain.sleep((lock_up_period+35) * ONE_DAY)
         chain.mine(2)
         assert isclose(
-            staking_contract.calculateUserReward(user_1),
+            staking_contract.calculateUserReward(user),
             (apy[str(lock_up_period)] * amount_to_stake * lock_up_period) / 365,
             abs_tol=10,
         )
         chain.revert()
 
-    def test_claimable_tokens(amount_to_stake):
-        # TODO: Removed a view from claimableTokens function since the chain.sleep does not work on view txns
+    def test_claimable_tokens(amount_to_stake, user):
         chain.snapshot()
         assert (
-            staking_contract.claimableTokens(user_1) == 0
+            staking_contract.claimableTokens(user) == 0
         ), "Incorrect claimable tokens"
         chain.sleep(15 * ONE_DAY)
         chain.mine(2)
         assert (
-            staking_contract.claimableTokens(user_1) == 0
+            staking_contract.claimableTokens(user) == 0
         ), "Incorrect claimable tokens"
         chain.sleep(30 * ONE_DAY)
         chain.mine(2)
         assert (
-            staking_contract.claimableTokens(user_1) == amount_to_stake
+            staking_contract.claimableTokens(user) == amount_to_stake
         ), "Incorrect claimable tokens"
         chain.sleep(95 * ONE_DAY)
         chain.mine(2)
         assert (
-            staking_contract.claimableTokens(user_1) == amount_to_stake
+            staking_contract.claimableTokens(user) == amount_to_stake
         ), "Incorrect claimable tokens"
         chain.revert()
 
-    def test_withdraw_flow(lock_up_period):
+    def test_withdraw_flow(lock_up_period, amount):
         # Withdraw before lock up period
         initial_tokenX_balance_of_staking_contract = tokenX.balanceOf(
             staking_contract.address
@@ -158,6 +157,7 @@ def test_abb_staking(contracts, accounts, chain):
         initial_total_staked_amount = staking_contract.totalStakedAmount()
         initial_user_balance = tokenX.balanceOf(user_1)
         claimable_tokens = staking_contract.claimableTokens(user_1)
+        user_reward = staking_contract.calculateUserReward(user_1)
         withdraw_transaction = staking_contract.withdraw(
             {"from": user_1}
         )
@@ -166,7 +166,6 @@ def test_abb_staking(contracts, accounts, chain):
         assert tokenX.balanceOf(
             staking_contract.address) == initial_tokenX_balance_of_staking_contract
         assert staking_contract.totalStakedAmount() == initial_total_staked_amount
-        assert staking_contract.calculateUserReward(user_1) == 0
 
         # Withdraw after lock up period
         chain.sleep(lock_up_period * ONE_DAY)
@@ -187,26 +186,83 @@ def test_abb_staking(contracts, accounts, chain):
         assert staking_contract.totalStakedAmount() == initial_total_staked_amount - amount
         assert withdraw_transaction.events["Withdraw"]["account"] == user_1
         assert withdraw_transaction.events["Withdraw"]["amount"] == amount
+        assert (
+            staking_contract.claimableTokens(user_1) == 0
+        ), "Incorrect claimable tokens"
 
+        
     # Flow before staking
     test_all_reward_calculations()
     test_user_flow_pre_staking()
 
-    # Staking flow testing
+    
+    ##### FLOW 1 : USER WITH ONE STAKES, WITHDRAWS THE STAKE AMOUNT
     lock_up_period = 30
-    test_initital_set_up(lock_up_period, transfer_amount)
-    test_staking_flow(lock_up_period, amount)
-    test_rewards(lock_up_period, amount)
-    test_claimable_tokens(amount)
+    chain.snapshot()
+    test_initital_set_up(transfer_amount, user_1)
+    test_staking_flow(lock_up_period, amount_1, user_1)
+    test_rewards(lock_up_period, amount_1, user_1)
+    test_claimable_tokens(amount_1, user_1)
+    traverse(user_1)
+    user_reward = staking_contract.calculateUserReward(user_1)
+    test_withdraw_flow(lock_up_period, amount_1)
+    traverse(user_1)
+    # Additional Checks
+    assert isclose(
+        staking_contract.calculateUserReward(user_1),
+        user_reward + (apy[str(lock_up_period)] * amount_1 * lock_up_period) / 365,
+        abs_tol=10,
+    )
+    chain.sleep(int(lock_up_period) * ONE_DAY)
+    chain.mine(2)
+    assert isclose(
+        staking_contract.calculateUserReward(user_1),
+        user_reward + (apy[str(lock_up_period)] * amount_1 * lock_up_period) / 365,
+        abs_tol=10,
+    )
+    chain.sleep(lock_up_period * ONE_DAY)
+    chain.mine(2)
+    assert (
+        staking_contract.claimableTokens(user_1) == 0
+    ), "Incorrect claimable tokens"
+    chain.revert()
+    
 
-    # Withdraw flow testing
-    test_withdraw_flow(lock_up_period)
+    # FLOW 2 : USER WITH MULTIPLE STAKES, WITHDRAWS THE FIRST STAKE AMOUNT
+    lock_up_period = 30
+    test_initital_set_up(transfer_amount,user_2)
+    test_staking_flow(lock_up_period, amount_1, user_2)
+    test_rewards(lock_up_period, amount_1, user_2)
+    test_claimable_tokens(amount_1, user_2)
+    test_staking_flow(60, amount_1, user_2)
+    test_staking_flow(60, amount_1, user_2)
+    test_staking_flow(90, amount_1, user_2)
+    traverse(user_2)
+    test_withdraw_flow(lock_up_period, amount_1)
+    traverse(user_2)
+    
+
+
+    # ALL PERMUTATIONS AND COMBINATIONS
+
+    # # Staking flow testing
+    # lock_up_period = 30
+    # amount_2 =  342 * int(1e18)
+    # test_staking_flow(lock_up_period, amount_2)
 
     # # Staking flow testing
     # lock_up_period = 60
-    # test_staking_flow(lock_up_period, amount)
+    # amount_3 =  563 * int(1e18)
+    # test_staking_flow(lock_up_period, amount_3)
 
     # # Staking flow testing
     # lock_up_period = 90
-    # test_staking_flow(lock_up_period, amount)
-    traverse(user_1)
+    # amount_4 = 863 * int(1e18)
+    # test_staking_flow(lock_up_period, amount_4)
+
+    # traverse(user_1)
+    
+    # chain.snapshot()
+    # lock_up_period = 30
+    # test_withdraw_flow(lock_up_period, amount_2)
+    # chain.revert()
